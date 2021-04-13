93cca33626b1e5eb77f486404e58b944ace5e049
skynote <tm@0728.im>
2021-04-13 09:27:26 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '79a2fa2fd33b4eaaae9989116aab297f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '机场分享',
+  domain: 'zhaoip.ml',
+  author: '机场分享',
 
   // open graph metadata (optional)
-  description: 'Example site description',
+  description: '免费机场分享、机场推荐',
   socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  socialImageSubtitle: '免费机场分享、机场推荐! 👋',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
