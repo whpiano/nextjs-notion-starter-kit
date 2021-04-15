7411eae9013a239dbb7734b26904540cdff6e809
sky <i@0728.im>
2021-04-15 22:02:33 +0800

    sns

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+      <div className={styles.copyright}>Copyright 2021 {config.author} · 本站基于notion搭建</div>
 
       {hasMounted ? (
         <div className={styles.settings}>
